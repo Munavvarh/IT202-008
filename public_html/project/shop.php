a568dc584027bdba491f24b4906021f3e62d75cf
Munavvarh <munavvarh123@gmail.com>
2023-04-25 16:50:36 -0400

    handled everything related to cart add/list/change/remove or go to details

--- public_html/project/shop.php
+++ public_html/project/shop.php
@@ -34,9 +34,9 @@ try {
                         <p class="card-text">Description: <?php se($item, "description"); ?></p>
                     </div>
                     <div class="card-footer">
-                        Cost: <?php se($item, "cost"); ?>
+                        Cost: <?php se($item, "unit_price"); ?>
                         <form method="POST" action="cart_alt.php">
-                            <input type="hidden" name="item_id" value="<?php se($item, "id"); ?>" />
+                            <input type="hidden" name="product_id" value="<?php se($item, "id"); ?>" />
                             <input type="hidden" name="action" value="add" />
                             <input type="number" name="desired_quantity" value="1" min="1" max="<?php se($item, "stock"); ?>" />
                             <input type="submit" class="btn btn-primary btn-block" style="width: 50%; font-size: 14px;" value="Add to Cart" />
